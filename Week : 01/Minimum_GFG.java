e1d629a5140f91f8ba9f593dbfdcb1496fab32a4
Kamendra Kushwah <kamendra.gla_cs23@gla.ac.in>
2025-09-16 22:00:00 +0530

    Create Minimum_GFG.java

---
+++ Week : 01/Minimum_GFG.java
@@ -0,0 +1,18 @@
+// User function Template for Java
+
+class Solution {
+    public int findMin(int[] arr) {
+        // complete the function here
+        int low=0;
+        int high=arr.length-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>arr[high]){
+                low=mid+1;
+            }else{
+                high=mid;
+            }
+        }
+        return arr[low];
+    }
+}
